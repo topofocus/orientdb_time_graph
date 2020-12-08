4a082ac51f87a49e47e9a83617c9ff433cb81764
Hartmut Bischoff <topofocus@gmaiol.com>
2020-12-08 17:06:48 +0000

    Support of multible locations of model-files

--- lib/orientdb_time_graph.rb
+++ lib/orientdb_time_graph.rb
@@ -13,20 +13,39 @@ module TG
 	# Is called from connect only if ActiveOrient.default_server is not set previously
 	# 
 	# otherwise the credentials from the main-activeorient-instance are used.
-  def self.set_defaults  login =  nil
+  def self.set_defaults  **login
     c = { :server =>  'localhost',
 	  :port   => 2480,
 	  :protocol => 'http',
 	  :user    => 'root',
 	  :password => 'root',
 	  :database => 'temp'
-    }.merge login.presence || {}
+    }.merge login
 
     ActiveOrient.default_server= { user: c[:user], password: c[:password] ,
 				   server: c[:server], port: c[:port]  }
     ActiveOrient.database = c[:database]
   end
 
+  def self.connect **login
+    project_root = File.expand_path('../..', __FILE__)
+    set_defaults( **login) unless ActiveOrient.default_server.is_a?(Hash) && ActiveOrient.default_server[:user].present?
+    ActiveOrient::Init.define_namespace { TG } 
+		# a provided block is used to introduce additional locations of model-files
+		the_model_dirs = block_given? ? [ "#{project_root}/model", yield].flatten :  [ "#{project_root}/model" ]
+			ActiveOrient::OrientDB.new  preallocate: true, model_dir: the_model_dirs
+  end
+
+  def self.check_and_initialize database_instance
+    if database_instance.get_classes( "name").values.flatten.include? 'time_base'
+      return true
+    else
+      TG::Setup.init_database database_instance
+      puts "Database-Structure allocated"
+      puts "Exiting now, please restart and call »TG::TimeGraph.populate«"
+      Kernel.exit
+    end
+	end
 
   def self.info 
     puts "-------------- TIME GRAPH ------------------"
--- lib/setup.rb
+++ lib/setup.rb
@@ -39,22 +39,4 @@ module TG
 
 
 
-  def self.connect **login
-    project_root = File.expand_path('../..', __FILE__)
-    set_defaults(login) unless ActiveOrient.default_server.is_a?(Hash) && ActiveOrient.default_server[:user].present?
-    ActiveOrient::Init.define_namespace { TG } 
-    ActiveOrient::Model.model_dir =  "#{project_root}/model"
-    ActiveOrient::OrientDB.new  preallocate: true  # connect via http-rest
-  end
-
-  def self.check_and_initialize database_instance
-    if database_instance.get_classes( "name").values.flatten.include? 'time_base'
-      return true
-    else
-      TG::Setup.init_database database_instance
-      puts "Database-Structure allocated"
-      puts "Exiting now, please restart and call »TG::TimeGraph.populate«"
-      Kernel.exit
-    end
-	end
 end
