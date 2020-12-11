65292458579b8b0216d951679fe61b2e7a16d6ba
Hartmut Bischoff <topofocus@gmaiol.com>
2020-12-11 18:52:32 +0000

    unifying format of connect.yml

--- lib/setup.rb
+++ lib/setup.rb
@@ -13,28 +13,26 @@ module TG
 	## Standalone setup 
 	##  
 	## initializes ActiveOrient and adds time-graph database-classes
-	## through namespace TG
+	## through namespace 
+	# 
+	# valid environments:  :test, :development, :production
 	def self.setup environment = :test
 
-	 project_root = File.expand_path('../..', __FILE__)
-	 connect_file = project_root +'/config/connect.yml'
+		read_yml = -> (key) do
+			YAML::load_file( File.expand_path('../../config/connect.yml',__FILE__))[key]
+		end
 
-	 databaseyml   = YAML.load_file( connect_file )[:orientdb][:database]
-	 admin_credentials = YAML.load_file(connect_file)[:orientdb][:admin]
-	 logon =  { database: databaseyml[environment],
-		 user: admin_credentials[:user],
-		 password: admin_credentials[:pass],
-		 server:  YAML.load_file( connect_file )[:orientdb][:server] }
 
-	 ActiveOrient::Init.connect  logon
-	 TG.connect logon
+	 logon =  read_yml[:orientdb][environment]
+
+	 ActiveOrient::Init.connect  **logon
+	 TG.connect **logon
 	 # we have to initialise the timegraph at this point, otherwise any
 	 # manual requirement fails.
 	 unless ActiveOrient::Model.namespace.send :const_defined?, 'Tag' 
 		 Setup.init_database V.orientdb
 	 end
 
-	 ActiveOrient::Model.model_dir =  project_root + '/model'
 	end
 
 
